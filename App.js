ce33e2396176a78313df693b6a654c2a607c7142
Devinawesley <Devinawesley@gmail.com>
2020-11-20 22:14:51 -0500

    Update App.js
    Added environment port and more paths I anticipate needing.

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
 
-app.listen(3001, () => {
-    console.log('Listening on port 3000')
+app.get('/login', (req, res) => {
+    res.send("login screen!")
+})
+
+app.get('/recipes', (req, res) => {
+    res.send("Recipe list!")
+})
+
+app.get('/recipes/create', (req, res) => {
+    res.send("Create a new recipe!")
+})
+
+app.get('/recipes/edit/:id', (req, res) => {
+    res.send(`Editing recipe with the ID of ${req.params.id}`)
+})
+
+app.listen(port || 3001, () => {
+    console.log(`listening on port ${port}`)
 })
